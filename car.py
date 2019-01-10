831c5c8f9bf5d6f572cc1ea28ae8cef7b4e5836a
Rakesh Rangaramu <rakeshrangaramu25@gmail.com>
2019-01-10 13:58:02 +0530

    car.py completed

---
+++ car.py
@@ -0,0 +1,13 @@
+class Car(object):
+
+    wheels = 4
+
+    def __init__(self, make, model):
+        self.make = make
+        self.model = model
+
+mustang = Car('Ford', 'Mustang')
+print(mustang.wheels)
+print(Car.wheels)
+print(mustang.make)
+print(mustang.model)
