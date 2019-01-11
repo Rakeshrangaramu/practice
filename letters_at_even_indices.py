5025f690d2304e4e52961d9e4245a7f4b75436a9
Rakesh Rangaramu <rakeshrangaramu25@gmail.com>
2019-01-12 01:00:51 +0530

    to find letters at even indices in a string modified

--- letters_at_even_indices.py
+++ letters_at_even_indices.py
@@ -4,9 +4,7 @@ str = input("Enter the string\n")
 a = []
 counter = 0
 for letter in str:
-    if (counter % 2) != 0:
-        str.replace('letter', '')
-    else:
+    if (counter % 2) == 0:
         a.append(letter)
     counter += 1
 print("".join(a))
