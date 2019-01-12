e1a485c4975b742ed063b5e8c43b1c39dfa8424f
Rakesh Rangaramu <rakeshrangaramu25@gmail.com>
2019-01-12 10:46:11 +0530

    adding list elements done (from practice problems)

--- add_list_elements.py
+++ add_list_elements.py
@@ -2,11 +2,16 @@
 
 a = int(input("enter the no. of elements in list\n"))
 b = []
+d = 0
 for element in range(a):
     c = int(input("enter the element\n"))
+    d = d+c
     b.append(c)
 
 print(b)
+print(d)
+
+# use this for sum of list
 print(sum(b))
 
 
