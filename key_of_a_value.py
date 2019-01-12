a7ed4db20bd5cc4200862bea898ca18119101aaa
Rakesh Rangaramu <rakeshrangaramu25@gmail.com>
2019-01-12 17:38:11 +0530

    key-value pair in given format(from practice problems)

---
+++ key_of_a_value.py
@@ -0,0 +1,6 @@
+# TODO:  python program to print all the key value pairs in a given dictionary in the given format
+
+dict = {'1':"Speckbit", '2':"World", '3':"Quiet"}
+for element in dict:
+    print(element+"--"+dict[element])
+
