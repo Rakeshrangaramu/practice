71802f3f6ed762db28e72c472b4386cc90c5a7c6
Rakesh Rangaramu <rakeshrangaramu25@gmail.com>
2019-01-14 15:54:53 +0530

    add key value pair to dict(from practice problems)

---
+++ add_to_dict.py
@@ -0,0 +1,6 @@
+# TODO: a python program to add a key value pair to the dictionary if the key does not exist in the given dictionary.
+d = {}
+for i in range(2):
+    c = input("enter the number of elements").split()
+    d[c[0]] = c[1]
+print(d)
