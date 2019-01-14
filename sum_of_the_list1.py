d34ceac1c81f119b6cae1a5754cd23101cf28635
Rakesh Rangaramu <rakeshrangaramu25@gmail.com>
2019-01-14 16:21:26 +0530

    sum of numbers in a list

---
+++ sum_of_the_list1.py
@@ -0,0 +1,9 @@
+# TODO: python function to sum all the numbers in a list.
+a = []
+c = []
+b = int(input("enter the number of elements to be added"))
+for element in range(b):
+    c = int(input("input element"))
+    a.append(c)
+print(a)
+print(sum(a))
