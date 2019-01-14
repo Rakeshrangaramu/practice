500144e9a5e90c1e44b01551cf4e33f0b720f618
Rakesh Rangaramu <rakeshrangaramu25@gmail.com>
2019-01-14 16:13:50 +0530

    adding words to list(from practice problems)

---
+++ add_words_to_list.py
@@ -0,0 +1,7 @@
+# TODO :function for adding the words to a list.
+c = []
+a = int(input("enter the number of words to be added"))
+for element in range(a):
+    b =input("enter the words: ")
+    c.append(b)
+print(c)
